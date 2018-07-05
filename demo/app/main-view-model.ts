1267bf7b4e3bb5c40607dabbc79dac9afbe62d65
Stefan Andres Charsley <charsleysa@gmail.com>
2018-07-05 20:19:54 +1200

    - Fix demo

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,14 +1,10 @@
 import { Observable } from 'tns-core-modules/data/observable';
-import { Mdc } from 'nativescript-mdc';
 
 export class HelloWorldModel extends Observable {
   public message: string;
-  private mdc: Mdc;
 
   constructor() {
     super();
-
-    this.mdc = new Mdc();
-    this.message = this.mdc.message;
+    this.message = 'hello';
   }
 }
