2ccb265aad5f8b51bcf89bc98a387bee57b36a82
Stefan Andres Charsley <charsleysa@gmail.com>
2018-09-11 20:45:37 +1200

    - Enable Angular AOT support

---
+++ src/components/bottomNavigation/angular/module.ngfactory.d.ts
@@ -0,0 +1,3 @@
+import * as i0 from '@angular/core';
+import * as i1 from './module';
+export declare const NativeScriptMDCBottomNavigationModuleNgFactory: i0.NgModuleFactory<i1.NativeScriptMDCBottomNavigationModule>;
